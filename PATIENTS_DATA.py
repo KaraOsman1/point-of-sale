7da0b78f2c96391093fb625078a6c118d1d0683c
KaraOsman1 <101851955+KaraOsman1@users.noreply.github.com>
2023-02-16 12:17:56 +0300

    Create PATIENTS_DATA.py

---
+++ PATIENTS_DATA.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'hospital'
+
+mysql = MySQL(app)
+
+@app.route('/add_patient', methods=['POST'])
+def add_patient():
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['age']
+        gender = request.form['gender']
+        address = request.form['address']
+        phone = request.form['phone']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO patients(name, age, gender, address, phone) VALUES(%s, %s, %s, %s, %s)", (name, age, gender, address, phone))
+        mysql.connection.commit()
+        cur.close()
+        return redirect('/')
